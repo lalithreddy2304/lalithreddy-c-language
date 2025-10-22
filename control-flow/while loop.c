18b3f2ca318a954a3b33cc30f236c764ce933610
lalithreddy2304 <250160226534.pedaballi@gdgu.org>
2025-10-22 20:39:49 +0530

    create while loop.c

---
+++ control-flow/while loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    printf("Numbers from 1 to 5:\n");
+    while (i <= 5) {
+        printf("%d ", i);
+        i++;
+    }
+
+    return 0;
+}
