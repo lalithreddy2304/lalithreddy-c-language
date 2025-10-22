82f9610a8efd152b458acc2d80a9f037bb032e97
lalithreddy2304 <250160226534.pedaballi@gdgu.org>
2025-10-22 20:49:40 +0530

    simple_function.c

---
+++ functions/recursions_example.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Function declaration
+void greet();
+
+int main() {
+    greet(); // Function call
+    return 0;
+}
+
+// Function definition
+void greet() {
+    printf("Hello from a function!\n");
+}
