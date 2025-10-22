8110cec09bf4c6d852e30500d7fcd9e34a19a5c8
lalithreddy2304 <250160226534.pedaballi@gdgu.org>
2025-10-22 20:40:44 +0530

    Create do_while loop.c

---
+++ control-flow/do_while loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    printf("Numbers from 1 to 3:\n");
+    do {
+        printf("%d ", i);
+        i++;
+    } while (i <= 3);
+
+    return 0;
+}
