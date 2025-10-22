f1915bb34b14faa2f35e3419f34cdf74fe8bcace
lalithreddy2304 <250160226534.pedaballi@gdgu.org>
2025-10-22 20:25:31 +0530

    add simple variables program

---
+++ basics/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 5.5;
+    char c = 'X';
+
+    printf("Integer: %d\n", a);
+    printf("Float: %.1f\n", b);
+    printf("Character: %c\n", c);
+
+    return 0;
+}
